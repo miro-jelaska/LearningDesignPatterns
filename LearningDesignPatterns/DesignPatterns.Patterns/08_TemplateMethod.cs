11d11a36671c78350ba03ad5d3e51456fe5bb97c
Miro Jelaska <git@jelaska.com>
2014-11-06 03:00:45 +0100

    Added TemplateMethod

---
+++ LearningDesignPatterns/DesignPatterns.Patterns/08_TemplateMethod.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Patterns.TemplateMethod
+{
+    public abstract class CaffeineBeverage
+    {
+        protected CaffeineBeverage()
+        {
+            this.boilWater();
+            this.Brew();
+            this.pourInCup();
+            this.AddCondiments();
+        }
+
+        private void boilWater()
+        {
+            Console.WriteLine("Boiling water...");
+        }
+        private void pourInCup()
+        {
+            Console.WriteLine("Pouring in cup...");
+        }
+        protected abstract void Brew();
+        protected abstract void AddCondiments();
+    }
+
+    class Coffe : CaffeineBeverage
+    {
+        public Coffe() : base() {}
+
+        protected override void Brew()
+        {
+            Console.WriteLine("Dripping Coffe through filter.");
+        }
+
+        protected override void AddCondiments()
+        {
+            Console.WriteLine("Adding sugar.");
+        }
+    }
+    class GreenTea : CaffeineBeverage
+    {
+        public GreenTea() : base() {}
+
+        protected override void Brew()
+        {
+            Console.WriteLine("Steeping the tea.");
+        }
+
+        protected override void AddCondiments()
+        {
+            Console.WriteLine(" - None to add. - ");
+        }
+    }
+
+    // Note: some TemplateMethods provide hook
+}
+namespace DesignPatterns.Patterns.TemplateMethod
+{
+    public abstract class Executor
+    {
+        public static void Execute()
+        {
+            var coffe = new Coffe();
+            var tea = new GreenTea();
+        }
+    }
+}
--- LearningDesignPatterns/LearningDesignPatterns/Program.cs
+++ LearningDesignPatterns/LearningDesignPatterns/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            DesignPatterns.Patterns.Facade.Executor.Execute();
+            DesignPatterns.Patterns.TemplateMethod.Executor.Execute();
 
             System.Threading.Thread.Sleep(15000);
         }
