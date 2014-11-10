312239a9e2be257b105cd1a958f293d100ba0d8a
Miro Jelaska <git@jelaska.com>
2014-11-10 02:13:43 +0100

    MinorChange

--- LearningDesignPatterns/LearningDesignPatterns/Program.cs
+++ LearningDesignPatterns/LearningDesignPatterns/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            DesignPatterns.Patterns.TemplateMethod.Executor.Execute();
+            DesignPatterns.Patterns.Iterator.Executor.Execute();
 
             System.Threading.Thread.Sleep(15000);
         }
